e15b8c9efe53017282129a25e538bdc4a3159fa6
AstrologiaMB <158606132+AstrologiaMB@users.noreply.github.com>
2025-07-27 11:12:04 -0300

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -14,8 +14,8 @@ const ses = new SES({
   apiVersion: "2010-12-01",
   region: "us-east-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 });
 
